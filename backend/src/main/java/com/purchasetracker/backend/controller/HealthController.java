dcca91f614cb6e9b8d81a4cb237669800e474e1a
Dhruv Kapadia <dhruvkapadia20@gmail.com>
2026-07-16 20:12:27 -0600

    Added CORS to allow Cross-origin requests. Updated HealthController to provide better status message. Updated frontend to call API health end point.

--- backend/src/main/java/com/purchasetracker/backend/controller/HealthController.java
+++ backend/src/main/java/com/purchasetracker/backend/controller/HealthController.java
@@ -2,17 +2,19 @@ package com.purchasetracker.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.time.Instant;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173") // Allow requests from the frontend
 public class HealthController {
 
     @GetMapping("/api/health") //GET request from /api/health
     public Map<String, Object> health() {
         return Map.of(
-                "status", "ok",
+                "status", "API up and running",
                 "service", "purchase-tracker-backend",
                 "timestamp", Instant.now().toString()
         );
